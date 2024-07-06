7e3264301e7c7d251c8d6f1f0922d733a132846b
Jared Roberts <roberts.a.jared@gmail.com>
2024-07-06 11:09:48 -0400

    Calculate fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,25 +1,60 @@
-import { useEffect, useState } from "react"
+import {useCallback, useEffect, useState } from "react"
+
+
+const calcCoffee : (water: number, ratio: number) => number  = (water, ratio) => {
+    return water * ratio;
+};
+
+const calcWater : (coffee: number, ratio: number) => number  = (coffee, ratio) => {
+    return coffee / ratio;
+};
 
 export default function Form() {
 
     const [state, setState] = useState({
+        Ratio: "0.0625",
+        Coffee: "",
+        Water: "",
     }) 
 
-    const handleChange = (e) => {
+    const handleChange = useCallback((e) => {
         
         const {target: {value, name}} = e;
 
-        console.log(value);
-        console.log(name);
+        switch (name) {
+            case "Ratio":
+                setState({
+                    ...state,
+                    Ratio: value,
+                    Water: calcWater(state.Coffee, value),
+                })
+                break;
+            case "Coffee":
+                setState({
+                    ...state,
+                    Coffee: value,
+                    Water: calcWater( value, state.Ratio),
+                })
+                break;
+            case "Water":  
+                setState({
+                    ...state,
+                    Water:value,
+                    Coffee: calcCoffee( value, state.Ratio),
+                })
+                break;
+            default:
+                setState({
+                    ...state,            
+                    [name]: value,
+        
+                })
+            break;
 
-        setState({
+        }
 
-            ...state,            
-            [name]:value,
 
-        })
-
-    }
+    }, [state])
 
     useEffect(()=> {
         console.log(state);
@@ -28,16 +63,8 @@ export default function Form() {
     return <div>
         <form onChange={handleChange} > 
             <label>
-                <span>Coffee</span>
-                <input type="number" name="Coffee" value={state.Coffee} />
-            </label>
-            <label>
-                <span>Water</span>
-                <input type="number" name="Water" value={state.Water ?? ""} />
-            </label>
-            <label>
                 <span>Ratio</span>
-                <select name="Ratio" value={state.Ratio ?? ""}>
+                <select name="Ratio" value={state.Ratio}>
                     <option value="0.0625">
                         1:16
                     </option>
@@ -47,11 +74,18 @@ export default function Form() {
                     <option value="0.05555555556">
                         1:18
                     </option>                    
+                    <option value="0.5">
+                        1:2
+                    </option>                    
                 </select>
             </label>
             <label>
-                <span>Yield</span>
-                <input type="number" name="Yield" value={state.Yield ?? ""}/>
+                <span>Coffee</span>
+                <input type="number" name="Coffee" value={state.Coffee ?? ""} />
+            </label>
+            <label>
+                <span>Water</span>
+                <input type="number" name="Water" value={state.Water ?? ""} />
             </label>
         </form>
     </div>
